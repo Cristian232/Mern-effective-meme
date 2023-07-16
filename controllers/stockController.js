c951829b6ce59fd7389e12e8d97d32d9ea7974c9
Cristian <Cristi@noemail.com>
2023-07-17 02:39:12 +0300

    Modified controllers for mongodb query

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,12 +1,8 @@
 import {nanoid} from "nanoid";
 import Stock from "../models/StockModel.js"
 
-let stocks = [
-    {id: nanoid(), company: "Google", ceo: "Sundar Pichai"},
-    {id: nanoid(), company: "Apple", ceo: "Tim Cook"}
-]
-
 export const getAllStocks = async (req, res) => {
+    const stocks = await Stock.find({})
     res.status(200).json({stocks})
 }
 
@@ -16,10 +12,9 @@ export const createStock = async (req, res) => {
         res.status(201).json({stock})
 }
 
-
 export const getStock = async (req, res) => {
     const {id} = req.params;
-    const stock = stocks.find((stock) => stock.id === id);
+    const stock = await Stock.findById(id);
     if (!stock) {
         return res.status(404).json({msg: `No such stock found ${id}`});
     }
@@ -27,27 +22,23 @@ export const getStock = async (req, res) => {
 }
 
 export const updateStock = async (req, res) => {
-    const {company, ceo} = req.body;
-    if (!company || !ceo) {
-        return res.status(400).json({msg: "Please provide company and ceo"})
-    }
     const {id} = req.params
-    const stock = stocks.find(stock => stock.id === id)
-    if (!stock) {
+    const updatedStock = await Stock.findByIdAndUpdate(id,req.body, {
+        new: true
+    })
+    if (!updatedStock) {
         return res.status(404).json({msg: `No company with ${id} found`})
     }
     stock.ceo = ceo;
     stock.company = company;
-    res.status(200).json({msg: "Stock modified", stock})
+    res.status(200).json({msg: "Stock modified", stocks: updatedStock})
 }
 
 export const deleteStock = async (req, res) => {
     const {id} = req.params
-    const stock = stocks.find(stock => stock.id === id)
-    if (!stock) {
+    const removedStock = await Stock.findByIdAndDelete(id)
+    if (!removedStock) {
         return res.status(404).json({msg: `No company with ${id} found`})
     }
-    const newStocks = stocks.filter((stock) => stock.id === id)
-    stocks = newStocks
-    res.status(200).json({msg: "Stock deleted"})
+    res.status(200).json({msg: "Stock deleted", stocks: removedStock})
 }
